30663495f75df4381f79e9fecdee01366fae5ce1
ayndecastro <vincentayndecastro@gmail.com>
2021-01-02 16:13:17 -0600

    building backend API, issue: amadeus API is limited to very little cities. need new api; considering google places API

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
+import os
 import flask
 import json
 import requests
 from flask import jsonify, request
+from dotenv import load_dotenv  # for python-dotenv method
+load_dotenv()
 
 jsonFile = open('db.json')
 api = json.load(jsonFile)
@@ -9,24 +12,47 @@ api = json.load(jsonFile)
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
-# imdb api link
-url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/game_of_"
 
-queryString = 'game of thr'
+@app.route('/api/token', methods=['POST'])
+def get_token():
+    url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+    client_id = os.environ.get('AMADEUS_API_KEY'),
+    client_secret = os.environ.get('AMADEUS_API_SECRET')
 
-headers = {
-    'x-rapidapi-key': "639a19526cmsh25c87cdd24fceafp19c5bbjsnddff03716412",
-    'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com"
-}
+    payload = f'client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-# search movies
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-
-@app.route('/api/search', methods=['GET'])
-def home():
-    response = requests.request(
-        "GET", url, headers=headers, params=queryString)
     return response.text
+
+
+@app.route('/api/search/<lat>/<lng>', methods=['GET'])
+def home(lat, lng):
+
+    url = f"https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude={lat}&longitude={lng}&radius=2"
+    print(url)
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer token'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+@app.route('/api/location/<location_query>', methods=['GET'])
+def geo_locator(location_query):
+    api_key = os.environ.get('GOOGLE_API_KEY')
+    gmaps_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location_query}&key={api_key}'
+    response = requests.request("GET", gmaps_url)
+    result = response.text
+
+    return result
+    # print(f"{os.environ.get(API_KEY)}")
 
 
 app.run()
