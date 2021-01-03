f6022efc391b54d5d08bc4ad04de752bcac14426
ayndecastro <vincentayndecastro@gmail.com>
2021-01-02 19:53:32 -0600

    api testing. to be updated with chosen api that will work the app design

--- api.py
+++ api.py
@@ -13,33 +13,12 @@ app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route('/api/token', methods=['POST'])
-def get_token():
-    url = "https://test.api.amadeus.com/v1/security/oauth2/token"
-    client_id = os.environ.get('AMADEUS_API_KEY'),
-    client_secret = os.environ.get('AMADEUS_API_SECRET')
+@app.route('/api/search/<place_id>', methods=['GET'])
+def home(place_id):
+    api_key = os.environ.get('GOOGLE_API_KEY')
+    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={api_key}"
 
-    payload = f'client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials'
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    return response.text
-
-
-@app.route('/api/search/<lat>/<lng>', methods=['GET'])
-def home(lat, lng):
-
-    url = f"https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude={lat}&longitude={lng}&radius=2"
-    print(url)
-    payload = {}
-    headers = {
-        'Authorization': 'Bearer token'
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url)
 
     return response.text
 
@@ -47,12 +26,12 @@ def home(lat, lng):
 @app.route('/api/location/<location_query>', methods=['GET'])
 def geo_locator(location_query):
     api_key = os.environ.get('GOOGLE_API_KEY')
-    gmaps_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location_query}&key={api_key}'
-    response = requests.request("GET", gmaps_url)
+    gmaps_url = f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={location_query}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key={api_key}'
+    payload = {}
+    response = requests.request("GET", gmaps_url, data=payload)
     result = response.text
 
     return result
-    # print(f"{os.environ.get(API_KEY)}")
 
 
 app.run()
